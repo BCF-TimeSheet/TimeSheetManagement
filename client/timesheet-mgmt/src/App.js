3380b0dd146c1d344fb0f158e2786fa5530aeff8
khoalan <khoalan96@gmail.com>
2022-02-06 17:39:42 -0500

    update nav bar

--- client/timesheet-mgmt/src/App.js
+++ client/timesheet-mgmt/src/App.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './components/Home'
-import AuthService from './services/auth.service'
-import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path={['/', '/home']} component={Home} />
-          <Route exact path="/login" component={Login} />
-          {/* <Route exact path="/profile" component={Profile} /> */}
-          {/* <Route path="/user" component={BoardUser} /> */}
-        </Switch>
-      </div>
+      <Home />
     </div>
   )
 }
--- client/timesheet-mgmt/src/components/Home.js
+++ client/timesheet-mgmt/src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import AuthService from '../services/auth.service'
-import UserService from '../services/user.service'
 import { Switch, Route, Link } from 'react-router-dom'
 import Login from './Login'
 
@@ -29,16 +28,29 @@ const Home = () => {
               Home
             </Link>
           </li>
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={'/summary'} className="nav-link">
+                Summary
+              </Link>
+            </li>
+          )}
 
           {currentUser && (
             <li className="nav-item">
-              <Link to={'/user'} className="nav-link">
-                User
+              <Link to={'/timesheet'} className="nav-link">
+                TimeSheet
+              </Link>
+            </li>
+          )}
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={'/profile'} className="nav-link">
+                Profile
               </Link>
             </li>
           )}
         </div>
-
         {currentUser ? (
           <div className="navbar-nav ml-auto">
             <li className="nav-item">
@@ -58,33 +70,77 @@ const Home = () => {
         )}
       </nav>
       {currentUser ? <h2>Welcome</h2> : <h2>Please log in</h2>}
+      <div className="container mt-3">
+        <Switch>
+          {/* <Route exact path={['/', '/home']} component={Home} /> */}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/summary" component={Login} />
+          <Route exact path="/timesheet" component={Login} />
+          <Route exact path="/profile" component={Login} />
+        </Switch>
+      </div>
     </div>
   )
-  //   const [content, setContent] = useState('')
-
-  //   useEffect(() => {
-  //     UserService.getPublicContent().then(
-  //       (response) => {
-  //         setContent(response.data)
-  //       },
-  //       (error) => {
-  //         const _content =
-  //           (error.response && error.response.data) ||
-  //           error.message ||
-  //           error.toString()
-
-  //         setContent(_content)
-  //       }
-  //     )
-  //   }, [])
-
-  //   return (
-  //     <div className="container">
-  //       <header className="jumbotron">
-  //         <h3>{content}</h3>
-  //       </header>
-  //     </div>
-  //   )
 }
 
 export default Home
+// <div>
+//   <nav className="navbar navbar-expand navbar-dark bg-dark">
+//     <div className="navbar-nav mr-auto">
+//       <li className="nav-item">
+//         <Link to={'/home'} className="nav-link">
+//           Home
+//         </Link>
+//       </li>
+//       {currentUser && (
+//         <li className="nav-item">
+//           <Link to={'/summary'} className="nav-link">
+//             Summary
+//           </Link>
+//         </li>
+//       )}
+//       {currentUser && (
+//         <li className="nav-item">
+//           <Link to={'/timesheet'} className="nav-link">
+//             TimeSheet
+//           </Link>
+//         </li>
+//       )}
+//       {currentUser && (
+//         <li className="nav-item">
+//           <Link to={'/profile'} className="nav-link">
+//             Profile
+//           </Link>
+//         </li>
+//       )}
+//     </div>
+
+//     {currentUser ? (
+//       <div className="navbar-nav ml-auto">
+//         <li className="nav-item">
+//           <a href="/login" className="nav-link" onClick={logOut}>
+//             LogOut
+//           </a>
+//         </li>
+//       </div>
+//     ) : (
+//       <div className="navbar-nav ml-auto">
+//         <li className="nav-item">
+//           <Link to={'/login'} className="nav-link">
+//             Login
+//           </Link>
+//         </li>
+//       </div>
+//     )}
+//   </nav>
+//   {currentUser ? <h2>Welcome</h2> : <h2>Please log in</h2>}
+//   <div className="container mt-3">
+//     <Switch>
+//       <Route exact path={['/', '/home']} component={Home} />
+//       <Route exact path="/login" component={Login} />
+//       <Route exact path="/summary" component={Login} />
+//       <Route exact path="/timesheet" component={Login} />
+//       <Route exact path="/profile" component={Login} />
+//     </Switch>
+//   </div>
+// </div>
